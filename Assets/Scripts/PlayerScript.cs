589c457ea9435e2851c94bdcd466a4eebb6708a5
Musomnia <jamesstuart3812@gmail.com>
2026-06-18 16:07:09 -0700

    Added jump input to player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,9 +5,13 @@ public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D body;
     public float moveSpeed;
+    public float jumpHeight;
 
     public InputActionReference move;
     private float moveDirection;
+
+    public InputActionReference jump;
+    private bool jumpReady = false;
     
     void Update()
     {
@@ -16,6 +20,32 @@ public class PlayerScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        body.linearVelocity = Vector3.right * moveDirection * moveSpeed;
+        body.linearVelocity = new Vector2(
+            moveDirection * moveSpeed,
+            body.linearVelocity.y
+        );
+
+        if (jump.action.IsPressed() && jumpReady)
+        {
+            body.linearVelocity = new Vector2(
+                body.linearVelocity.x,
+                jumpHeight
+            );
+
+            jumpReady = false;
+        } 
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.normal.y > 0.9f) jumpReady = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        jumpReady = false;
     }
 }
